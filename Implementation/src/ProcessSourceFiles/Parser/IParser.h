190d0985777ef5a736033d51ad9a28c525ef1c82
Hugo Torquato <hugortorquato@gmail.com>
2025-07-24 08:39:38 -0300

    Implemented boder of parser class, it can loop all tokens now with unit tests

---
+++ Implementation/src/ProcessSourceFiles/Parser/IParser.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <memory>
+#include <vector>
+
+#include "../Scanner/IToken.h"
+
+class IParser
+{
+   public:
+    virtual ~IParser() = default;
+
+    virtual std::vector<std::string> ListIncomingTokens() const = 0;
+};
