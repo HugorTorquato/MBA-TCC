ebf774c3fee7bb7c46ab637db3dae6d185140497
Hugo Torquato <hugortorquato@gmail.com>
2025-08-06 20:41:37 -0300

    Build is now incremental. Which reduces the time between tests

--- Implementation/scripts/build_and_run.sh
+++ Implementation/scripts/build_and_run.sh
@@ -1,15 +1,48 @@
 #!/bin/bash
 
-echo "<> Cleaning and setting up build directory..."
-rm -rf ../build
-mkdir ../build
-cd ../build
+# echo "<> Cleaning and setting up build directory..."
+# rm -rf ../build
+# mkdir ../build
+# cd ../build
 
-# echo "<> Running CMake..."
-# cmake ..
+# # echo "<> Running CMake..."
+# # cmake ..
 
-echo "<> Use ASAN (AddressSanitizer) to catch the exact location:..."
-cmake -DCMAKE_CXX_FLAGS="-fsanitize=address -g" ..
+# echo "<> Use ASAN (AddressSanitizer) to catch the exact location:..."
+# cmake -DCMAKE_CXX_FLAGS="-fsanitize=address -g" ..
+
+# echo "<> Running Auto-format..."
+# make format
+
+# echo "<> Building project..."
+# make -j$(nproc)
+
+# echo "<> Build finished."
+
+# echo "<> Running unit tests..."
+# ./my_tests
+
+
+
+
+BUILD_DIR="../build"
+
+if [[ "$1" == "--full" ]]; then
+    echo "<> Performing full clean build..."
+    rm -rf $BUILD_DIR
+    mkdir -p $BUILD_DIR
+else
+    echo "<> Performing incremental build..."
+    mkdir -p $BUILD_DIR
+fi
+
+cd $BUILD_DIR || exit 1
+
+# Only rerun CMake if no Makefiles yet
+if [ ! -f Makefile ]; then
+    echo "<> Running CMake..."
+    cmake -DCMAKE_CXX_FLAGS="-fsanitize=address -g" ..
+fi
 
 echo "<> Running Auto-format..."
 make format
@@ -19,9 +52,11 @@ make -j$(nproc)
 
 echo "<> Build finished."
 
-echo "<> Running unit tests..."
-./my_tests
+# echo "<> Running unit tests..."
+# ./my_tests
+
 
 # echo "<> Starting the API..."
 # pkill api_server
 # ./api_server
+
