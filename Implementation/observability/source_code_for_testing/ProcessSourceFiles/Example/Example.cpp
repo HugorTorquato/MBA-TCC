9f910474bb3adfd91765c507da562fcaf05e4ff7
Hugo Torquato <hugortorquato@gmail.com>
2025-09-09 08:13:42 -0300

    Upload Example function test case

---
+++ Implementation/observability/source_code_for_testing/ProcessSourceFiles/Example/Example.cpp
@@ -0,0 +1,15 @@
+// Example.cpp
+class Animal {
+public:
+    void speak();
+};
+
+class Dog : public Animal {
+public:
+    void wagTail();
+};
+
+class Cat : public Animal {
+public:
+    void purr();
+};
